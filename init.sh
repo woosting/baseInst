6b7ca0b62601556fc0615e84f060d2db507ecea3
Willem Oosting <woosting@users.noreply.github.com>
2016-08-27 17:57:26 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+USER=$1
+#
+apt-get update && apt-get install -y dist-upgrade
+apt-get install -y vim colordiff screen
+adduser --disabled password --gecos "${USER}" ${USER}
+usermod -aG sudo ${USER}
+mkdir /home/${USER}/.ssh/ && touch /home/${USER}/.ssh/authorized_keys
+chown ${USER}:${USER} -R /home/${USER}/.ssh && chmod 600 -R /home/${USER}/.ssh
