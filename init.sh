6d7e712510ef46ce3d9bf12b3c759e8284a0916d
Willem Oosting <woosting@users.noreply.github.com>
2016-08-28 08:57:29 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,22 +2,22 @@
 #
 USER=$1
 #
-# UPDATE + UPGRADE
+# UPDATE + UPGRADE + INSTALLS
   apt-get update && apt-get install -y dist-upgrade
   apt-get install -y vim colordiff screen linuxlogo
-# USER SPECIFICS
+  apt-get install -y linuxlogo
+# USER ADDITION
   adduser --disabled-password --gecos "${USER}" ${USER}
   usermod -aG sudo ${USER}
   mkdir /home/${USER}/.ssh/ && touch /home/${USER}/.ssh/authorized_keys
   chown ${USER}:${USER} -R /home/${USER}/.ssh && chmod 700 /home/${USER}/.ssh && chmod 600 /home/${USER}/.ssh/authorized_keys
   echo "" >> /home/${USER}/.bashrc
-  echo "" >> /home/${USER}/.bashrc
   echo "###" >> /home/${USER}/.bashrc
   echo "# INIT.SH ADDITIONS" >> /home/${USER}/.bashrc
   echo "# Grouping directories first:" >> /home/${USER}/.bashrc
-  echo "  ls='ls --color=auto --group-directories-first'" >> /home/${USER}/.bashrc
+  echo "alias ls='ls --color=auto --group-directories-first'" >> /home/${USER}/.bashrc
   echo "# Display logo at bash login:" >> /home/${USER}/.bashrc
-  echo "  if [ -f /usr/bin/linux_logo ]; then linuxlogo -u -y; fi" >> /home/${USER}/.bashrc
-  echo "" >> /home/${USER}/.bashrc
+  #echo "if [ -f /usr/bin/linux_logo ]; then linuxlogo -u -y; fi" >> /home/${USER}/.bashrc
+  #echo "" >> /home/${USER}/.bashrc
   echo "###" >> /home/${USER}/.bashrc
   echo "# MANUAL ADDITIONS" >> /home/${USER}/.bashrc
