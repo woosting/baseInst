cbbcec544009e8036daf68a2a55585ef2ade9764
Willem Oosting <woosting@users.noreply.github.com>
2016-08-28 02:12:48 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,4 +7,4 @@ apt-get install -y vim colordiff screen
 adduser --disabled-password --gecos "${USER}" ${USER}
 usermod -aG sudo ${USER}
 mkdir /home/${USER}/.ssh/ && touch /home/${USER}/.ssh/authorized_keys
-chown ${USER}:${USER} -R /home/${USER}/.ssh && chmod 600 -R /home/${USER}/.ssh
+chown ${USER}:${USER} -R /home/${USER}/.ssh && chmod 700 /home/${USER}/.ssh && chmod 600 /home/${USER}/.ssh/authorized_keys
