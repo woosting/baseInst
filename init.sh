39e1eacbca98cce6981738f9c94aec93777a4758
Willem Oosting <woosting@users.noreply.github.com>
2016-09-17 11:26:39 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 TODAY=$(date +%Y%m%d)
-USER=""
+NEWUSER=""
 
 while getopts u:d option
   do
     case "${option}"
      in
-      u) USER=(${OPTARG});;
+      u) NEWUSER=(${OPTARG});;
       d) DENV="lxde";;
       #x) EXAMPLE=(${OPTARG});;
     esac
   done
 
-if [ ! ${USER} ]; then
+if [ ! ${NEWUSER} ]; then
       echo -e "Usage: init [-d] -u username"
       echo -e "       -u Creates the user (REQUIRED)"
       echo -e "       -d Sets desktop environment to: LXDE (OPTIONAL)"
@@ -24,42 +24,42 @@ if [ ! ${USER} ]; then
       apt-get install -y vim screen git colordiff
       #apt-get install -y linuxlogo
     # USER ADDITION
-      adduser --disabled-password --gecos "${USER}" ${USER}
-      usermod -aG sudo ${USER}
-      mkdir /home/${USER}/.ssh/ && touch /home/${USER}/.ssh/authorized_keys
-      chown ${USER}:${USER} -R /home/${USER}/.ssh && chmod 700 /home/${USER}/.ssh && chmod 600 /home/${USER}/.ssh/authorized_keys
+      adduser --disabled-password --gecos "${NEWUSER}" ${NEWUSER}
+      usermod -aG sudo ${NEWUSER}
+      mkdir /home/${NEWUSER}/.ssh/ && touch /home/${NEWUSER}/.ssh/authorized_keys
+      chown ${NEWUSER}:${NEWUSER} -R /home/${NEWUSER}/.ssh && chmod 700 /home/${NEWUSER}/.ssh && chmod 600 /home/${NEWUSER}/.ssh/authorized_keys
     # CUSTOM SCRIPTS
-      mkdir /home/${USER}/scripts
-      git clone https://github.com/woosting/dirp.git /home/${USER}/scripts/dirp && \
-        ln -s /home/${USER}/scripts/dirp/dirp.sh /usr/local/bin/dirp
-      git clone https://github.com/woosting/stba.git /home/${USER}/scripts/stba && \
-        ln -s /home/${USER}/scripts/stba/stba.sh /usr/local/bin/stba
-      chown ${USER}:${USER} -R /home/${USER}/scripts
-      chmod 755 /home/${USER}/scripts/*/*.sh
+      mkdir /home/${NEWUSER}/scripts
+      git clone https://github.com/woosting/dirp.git /home/${NEWUSER}/scripts/dirp && \
+        ln -s /home/${NEWUSER}/scripts/dirp/dirp.sh /usr/local/bin/dirp
+      git clone https://github.com/woosting/stba.git /home/${NEWUSER}/scripts/stba && \
+        ln -s /home/${NEWUSER}/scripts/stba/stba.sh /usr/local/bin/stba
+      chown ${NEWUSER}:${NEWUSER} -R /home/${NEWUSER}/scripts
+      chmod 755 /home/${NEWUSER}/scripts/*/*.sh
     # UX
-      cp /home/${USER}/.bashrc /home/${USER}/.bashrc.bak${TODAY}
-      echo "" >> /home/${USER}/.bashrc
-      echo "###" >> /home/${USER}/.bashrc
-      echo "# INIT.SH ADDITIONS" >> /home/${USER}/.bashrc
-      echo "# Things automatically added by the init.sh script:" >> /home/${USER}/.bashrc
-      echo "# Grouping directories first:" >> /home/${USER}/.bashrc
-      echo "alias ls='ls --color=auto --group-directories-first'" >> /home/${USER}/.bashrc
-      echo "alias weather='wget -qO- wttr.in'" >> /home/${USER}/.bashrc
-      echo "alias cpuhoggers='ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head && uptime'" >> /home/${USER}/.bashrc
-      echo "alias memhoggers='ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head'" >> /home/${USER}/.bashrc
-      echo "" >> /home/${USER}/.bashrc
-      echo "###" >> /home/${USER}/.bashrc
-      echo "# MANUAL ADDITIONS" >> /home/${USER}/.bashrc
-      echo "# Add your own:" >> /home/${USER}/.bashrc
-      echo "" >> /home/${USER}/.bashrc
-      echo "###" >> /home/${USER}/.bashrc
-      echo "# LAST TO EXECUTE" >> /home/${USER}/.bashrc
-      echo "# Things to run at the end of .bashrc loading:" >> /home/${USER}/.bashrc
-      echo "# Display logo at bash login:" >> /home/${USER}/.bashrc
-      echo "#if [ -f /usr/bin/linux_logo ]; then linuxlogo -u -y; fi" >> /home/${USER}/.bashrc
+      cp /home/${NEWUSER}/.bashrc /home/${NEWUSER}/.bashrc.bak${TODAY}
+      echo "" >> /home/${NEWUSER}/.bashrc
+      echo "###" >> /home/${NEWUSER}/.bashrc
+      echo "# INIT.SH ADDITIONS" >> /home/${NEWUSER}/.bashrc
+      echo "# Things automatically added by the init.sh script:" >> /home/${NEWUSER}/.bashrc
+      echo "# Grouping directories first:" >> /home/${NEWUSER}/.bashrc
+      echo "alias ls='ls --color=auto --group-directories-first'" >> /home/${NEWUSER}/.bashrc
+      echo "alias weather='wget -qO- wttr.in'" >> /home/${NEWUSER}/.bashrc
+      echo "alias cpuhoggers='ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head && uptime'" >> /home/${NEWUSER}/.bashrc
+      echo "alias memhoggers='ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head'" >> /home/${NEWUSER}/.bashrc
+      echo "" >> /home/${NEWUSER}/.bashrc
+      echo "###" >> /home/${NEWUSER}/.bashrc
+      echo "# MANUAL ADDITIONS" >> /home/${NEWUSER}/.bashrc
+      echo "# Add your own:" >> /home/${NEWUSER}/.bashrc
+      echo "" >> /home/${NEWUSER}/.bashrc
+      echo "###" >> /home/${NEWUSER}/.bashrc
+      echo "# LAST TO EXECUTE" >> /home/${NEWUSER}/.bashrc
+      echo "# Things to run at the end of .bashrc loading:" >> /home/${NEWUSER}/.bashrc
+      echo "# Display logo at bash login:" >> /home/${NEWUSER}/.bashrc
+      echo "#if [ -f /usr/bin/linux_logo ]; then linuxlogo -u -y; fi" >> /home/${NEWUSER}/.bashrc
     if [ ${DENV} ]; then
-      mkdir /home/${USER}/Downloads/${DENV}
-      mkdir /home/${USER}/Downloads/${DENV}/openbox && wget -P /home/${USER}/Downloads/${DENV}/openbox https://dl.opendesktop.org/api/files/download/id/1460769323/69196-1977-openbox.obt
-      mkdir /home/${USER}/Downloads/${DENV}/icons && wget -P /home/${USER}/Downloads/${DENV}/icons https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/faenza-icon-theme/faenza-icon-theme_1.3.zip
+      mkdir /home/${NEWUSER}/Downloads/${DENV}
+      mkdir /home/${NEWUSER}/Downloads/${DENV}/openbox && wget -P /home/${NEWUSER}/Downloads/${DENV}/openbox https://dl.opendesktop.org/api/files/download/id/1460769323/69196-1977-openbox.obt
+      mkdir /home/${NEWUSER}/Downloads/${DENV}/icons && wget -P /home/${NEWUSER}/Downloads/${DENV}/icons https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/faenza-icon-theme/faenza-icon-theme_1.3.zip
     fi
 fi
